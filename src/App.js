68fa4aa10913bbfab07cec2c9a3f7a39daff0321
Villalba <lmv@a483e7ad5050.ant.amazon.com>
2021-11-09 13:58:25 +0200

    drawing the map

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import Amplify from 'aws-amplify';
+import { AmplifyAuthenticator } from '@aws-amplify/ui-react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { useEffect, useState } from 'react';
+
+import Header from './components/Header'
+import Map from './components/Map'
+
+import awsconfig from './aws-exports';
+
+import { createMap } from "maplibre-gl-js-amplify"; 
+import "maplibre-gl/dist/maplibre-gl.css";
+
+Amplify.configure(awsconfig);
+
+
+//* ---- CONSTANTS ----- */
+
+const INITIAL_VIEWPORT = {
+  longitude: -56.164532,
+  latitude: -34.901112,
+}
+
+
+const App = () => {
+
+  const [map, setMap] = useState();
+
+  useEffect(() => {
+    const initializeMap = async () => {
+        setMap( 
+        await createMap({
+            container: "map", // An HTML Element or HTML element ID to render the map in https://maplibre.org/maplibre-gl-js-docs/api/map/
+            center: [INITIAL_VIEWPORT.longitude, INITIAL_VIEWPORT.latitude],
+            zoom: 13,
+        })
+    )};
+
+    initializeMap();
+}, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AmplifyAuthenticator>
+      <div className="App">
+        <Header />
+        <Map myMap={map}/>
+
+      </div>
+    </AmplifyAuthenticator>
   );
 }
 
---
+++ src/components/Map.js
@@ -0,0 +1,14 @@
+
+
+function Map(props) {
+    console.log(props.myMap)
+    return (
+        <div className="container">
+            <div id="map">
+            </div>
+        </div>
+    );
+  }
+
+
+export default Map;
